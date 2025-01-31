3fab72aa1dcaaed5d42e610610495376f1cad9e7
wamash-82 <kamanderuth46@gmail.com>
2025-01-31 12:37:24 +0300

    python project

---
+++ Basics.py
@@ -0,0 +1,26 @@
+#display text
+print("Hello welcome to my first python class.")
+print("I hope it's gonna be easy to learn and fun.")
+print("Especially now that's it's heading to a weekend I hope my concentration span will cooperate.")
+
+#variables
+fname="Fifi"
+lname="Wamash"
+
+#print the values
+print(fname)
+print(lname)
+
+
+#variables
+coursename="Fullstack"
+studentname="Wamaitha"
+classname="Chrome lab"
+teachername="Faith"
+
+
+#print the values
+print(coursename)
+print(studentname)
+print(classname)
+print(teachername)
