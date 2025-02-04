8e9450dacedf9ea3345fcd645ffcb94eb4580cbd
wamash-82 <kamanderuth46@gmail.com>
2025-02-04 13:07:03 +0300

    python

---
+++ Numbers.py
@@ -0,0 +1,35 @@
+#Numbers: integers, float, complex
+#Integers; whole numbers(+ve/-ve)
+x=2000
+y=77
+z=-540
+print(type(x))
+print(type(y))
+print(type(z))
+#Float - numbers with dp
+a=10.45
+b=99.99
+print(type(a))
+print(type(b))
+#Complex; numbers with unknown part
+#j is used to show the imaginary part
+c=2+5j
+d=6j+3
+print(type(c))
+print(type(d))
+#Type conversion float(), int(), complex()
+#From integers to float
+e=37
+mynum=float(e)
+print(mynum)
+print(type(mynum))
+#convert from float to int
+my=19.45
+new=int(my)
+print(new)
+print(type(new))
+#Convert int to complex
+f=645
+wa=complex(f)
+print(wa)
+print(type(wa))
---
+++ Operators.py
@@ -0,0 +1,49 @@
+fname="Wanjiru"
+hobbies="Hiking"
+print("My name is", fname, "I love", hobbies)
+#f string
+print(f"My name is {fname} I love {hobbies}")
+#Arithmetic Operators + - * / %
+#Addition
+x=13
+y=27
+ans=x+y
+print(f"Addition of {x} and {y} is {ans}")
+# -
+print(y-x)
+print(f"Diff of {y} amd {x} is {y-x}" )
+#Multiplication *
+prod=x*y
+print(f"Product of {x} and {y} is {prod}")
+#Divison
+print(y/x)
+#Modules:The remainder of division %
+a=5
+b=2
+print(f"Modulus {a} and {b} is {a%b} ")
+#Assignement operators = += -= they assign values to variables
+d=10
+print(d)
+d+=5
+print(d)
+#Comparison operators;used for comparing 2 values
+#Equals ==
+print(x==y)
+#Greater than
+print(x>y)
+#Less than <
+print(x<y)
+#Greater than or equal to >=
+print(y>=x)
+#Less than or equal to <=
+print(x<=y)
+#Logical operators and,or,not
+#And;Returns true if both statements are true
+u=5
+print(f"{u>3 and u>10}")
+#or:returns true if one statement is true
+print(u<3 or u<10)
+#not reverses the result
+f=True
+print(f)
+print(not (f))
